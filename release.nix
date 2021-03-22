6852125b17f0f748264b2c2b7f53c1f28541cc81
John Ericson <John.Ericson@Obsidian.Systems>
2021-03-22 13:18:46 -0400

    Fix release.nix

--- release.nix
+++ release.nix
@@ -1,4 +1,4 @@
 { }:
 let platforms = [ "x86_64-linux" "x86_64-darwin" ];
     lib = (import (import ./dep/nixpkgs.nix) {}).lib;
-in map (system: import ./project.nix { inherit system; }) platforms
+in map (system: import ./. { inherit system; }) platforms
